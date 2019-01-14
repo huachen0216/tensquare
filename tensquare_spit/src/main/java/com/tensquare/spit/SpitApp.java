1b88c84d0f60e4b423153099908c7839944c8b16
huachen <huachen0216@gmail.com>
2019-01-15 00:17:38 +0800

    增加了mongodb组件

---
+++ tensquare_spit/src/main/java/com/tensquare/spit/SpitApp.java
@@ -0,0 +1,20 @@
+package com.tensquare.spit;
+
+import com.fathua.util.IdWorker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpitApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpitApp.class, args);
+        System.out.println("==========SpritApp Startup=============");
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(1, 1);
+    }
+
+}
